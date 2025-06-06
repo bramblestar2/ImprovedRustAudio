cd4d5739381907c5ef02df00f96997b5c75ab65b
Jay Feather2000 <thegals4321@gmail.com>
2025-06-06 03:48:39 -0500

    a

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::{fs, path::Path};
+
+fn main() {
+    println!("cargo:rerun-if-changed=src/api.rs");
+
+    cxx_build::bridge("src/api.rs")
+        .flag_if_supported("-std=c++23")
+        .compile("audio");
+
+    let cxx_path = Path::new("target/cxxbridge/rust/cxx.h");
+    let api_h_path = Path::new("target/cxxbridge/improved_rust_audio/src/api.rs.h");
+    let api_cc_path = Path::new("target/cxxbridge/improved_rust_audio/src/api.rs.cc");
+
+    fs::create_dir_all("include").unwrap();
+    fs::copy(cxx_path, "include/cxx.h").expect("Failed to copy cxx.h");
+    fs::copy(api_h_path, "include/audio.h").expect("Failed to copy api.h");
+    fs::copy(api_cc_path, "include/audio.cc").expect("Failed to copy api.cc");
+}
